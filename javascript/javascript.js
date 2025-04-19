c9f036eb381d5cfa166ffefaf4da2909261a793e
aiiraRobertson <arthurifiokobong@gmail.com>
2025-04-19 13:12:56 -0700

    Refactor footer structure and styles; move JavaScript to separate file for better organization

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,12 @@
-var sum = 0;
+var left = 0;
+
+function frame() {
+  var element = document.querySelector(".item-relative");
+  left += 2;
+  element.style.left = left + "px";
+  if (left >= 300) {
+    clearInterval(id);
+  }
+}
+
+var id = setInterval(frame, 10);
